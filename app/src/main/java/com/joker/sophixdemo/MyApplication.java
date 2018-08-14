bffabbcfd55f354a55408973b4b8dab86427f927
PengXJ <99133950@qq.com>
2018-08-14 17:37:27 +0800

    Initial commit

---
+++ app/src/main/java/com/joker/sophixdemo/MyApplication.java
@@ -0,0 +1,16 @@
+package com.joker.sophixdemo;
+
+import android.app.Application;
+import android.support.annotation.Keep;
+
+/**
+ * Created by tv on 2018/8/14.
+ */
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
